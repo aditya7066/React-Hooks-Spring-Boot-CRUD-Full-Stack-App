c8594293885e708ef4e03732afdf022f95e99a8f
aditya7066 <95438053+aditya7066@users.noreply.github.com>
2024-05-22 11:51:34 +0530

    Add files via upload

---
+++ Reactjs-Hooks-Frontend/src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className="footer">
+            <span className="text-muted">All Right Reserved 2024</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
